63efd23e689850189a125b6ed567a027b4620239
徐志豪 <1505891112@qq.com>
2022-09-06 16:13:09 +0800

    feat:配置完毕

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,18 @@
 <template>
-<div>黑马头条</div>
+<div>黑马头条
+    <van-button type="primary">主要按钮</van-button>
+    <div class="box"></div>
+</div>
 </template>
 
 <script>
 export default {}
 </script>
 
-<style></style>
+<style>
+.box {
+    background-color: red;
+    width: 2rem;
+    height: 2rem;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -2,11 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+// 引入vant
+import Vant from 'vant'
+import 'vant/lib/index.css'
+// 引入 amfe-flexible  (1.让ren动起来 npm i amfe-flexible 2.盒子使用rem)
+import 'amfe-flexible'
+Vue.use(Vant)
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: (h) => h(App)
 }).$mount('#app')
